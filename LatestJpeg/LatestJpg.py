16d4e42162ce2cf662b1473e8f2cf7bcdaa73d94
Tim Seed <du3tw@sy-edm.com>
2024-10-22 08:48:19 +0800

    Slight mods

--- LatestJpeg/LatestJpg.py
+++ LatestJpeg/LatestJpg.py
@@ -1,7 +1,7 @@
 import operator
 from stat import ST_CTIME
 import os, sys, time
-
+from glob import glob
 
 class LatestJpg:
 
@@ -9,18 +9,17 @@ class LatestJpg:
         if mydir is None:
             self.path = str(os.getcwd()) + '/'
         else:
-            os.chdir(mydir)
-            self.path = str(os.getcwd()) + '/'
 
-        self.files = os.listdir(self.path)
+            self.path = mydir
 
     def mostrecentfile(self):
-        all_files = os.listdir(self.path)
+        all_files = glob(self.path+"/**/*.jpg") + glob(self.path+"/**/*.jpeg")
+
         file_times = dict()
         for e in all_files:
             filename, file_extension = os.path.splitext(e)
             file_extension = file_extension.strip().upper()
-            if file_extension in ["JPG","JPEG"]:
+            if file_extension in [".JPG",".JPEG"]:
                 file_times[e] = time.time() - os.stat(e).st_ctime
         if len(file_times) > 0:
             return sorted(file_times.items(), key=operator.itemgetter(1))[0][0]
--- Readme.md
+++ Readme.md
@@ -5,3 +5,11 @@ A Simple python script to find the latest file in a directory path.
 **note**: This is not asynchronious - it needs to be called each time to detect a file file. 
 
 I believe *watchdog* will provide Asynch file scanning.
+
+## Example useage 
+
+```python
+from LatestJpeg import LatestJpg
+lj = LatestJpg("/Users/DrSpok/Dev/Astro/Images")
+print(f"Most recent file is {lj.mostrecentfile()}")
+```      
